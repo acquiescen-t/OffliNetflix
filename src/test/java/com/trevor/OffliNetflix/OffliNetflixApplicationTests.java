f6a4e1b79ab888e240ff2fcbf0fa561ce2b41992
Trevor <trevorchew.kh@gmail.com>
2024-09-23 15:51:02 +0800

    Initial commit

---
+++ src/test/java/com/trevor/OffliNetflix/OffliNetflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trevor.OffliNetflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OffliNetflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
