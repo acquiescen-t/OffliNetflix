6bd9fde3a3b4ed2942ee0b3a45c00973722c25b3
Trevor <trevorchew.kh@gmail.com>
2024-09-25 17:48:40 +0800

    Genre classes
    Setup database to hold genre relation information

---
+++ src/main/java/com/trevor/OffliNetflix/Genre/Genre.java
@@ -0,0 +1,58 @@
+package com.trevor.OffliNetflix.Genre;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class Genre {
+
+    @Id
+    @SequenceGenerator(
+            name = "genre_sequence",
+            sequenceName = "genre_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "genre_sequence"
+    )
+    private Long id;
+    private String name;
+
+    public Genre() {
+
+    }
+
+    public Genre(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Genre(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Genre{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/trevor/OffliNetflix/Genre/GenreRepository.java
@@ -0,0 +1,6 @@
+package com.trevor.OffliNetflix.Genre;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GenreRepository extends JpaRepository<Genre, Long> {
+}
