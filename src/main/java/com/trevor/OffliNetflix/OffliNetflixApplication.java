09f5d9236016b64e401553e53e0a53ed4b02caff
Trevor <trevorchew.kh@gmail.com>
2024-09-25 17:52:53 +0800

    Enumerate from movie directory

--- src/main/java/com/trevor/OffliNetflix/OffliNetflixApplication.java
+++ src/main/java/com/trevor/OffliNetflix/OffliNetflixApplication.java
@@ -1,9 +1,18 @@
 package com.trevor.OffliNetflix;
 
+import com.trevor.OffliNetflix.Film.Film;
+import com.trevor.OffliNetflix.Util.ExtensionFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class OffliNetflixApplication {
@@ -13,11 +22,15 @@ public class OffliNetflixApplication {
 		SpringApplication.run(OffliNetflixApplication.class, args);
 	}
 
-	public static void EnumerateDirectory() throws InterruptedException {
-		File dir = new File("");
-		File[] filesInDir = dir.listFiles();
-		if (filesInDir != null)
-			for (File f : filesInDir)
+	public static void EnumerateDirectory() {
+		File rootDirectory = new File(Film.getRootDir());
+		List<File> movieDirectories = List.of(rootDirectory.listFiles());
+
+		if (movieDirectories != null)
+			for (File f : movieDirectories)
 				System.out.println(f.getPath());
+
 	}
+
+
 }
