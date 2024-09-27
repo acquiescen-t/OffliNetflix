e17a914e511ccc5fc92bb6ecc96a1d43ee9e053e
Trevor <trevorchew.kh@gmail.com>
2024-09-27 16:44:34 +0800

    Test walking through directory to extract film details

--- src/main/java/com/trevor/OffliNetflix/OffliNetflixApplication.java
+++ src/main/java/com/trevor/OffliNetflix/OffliNetflixApplication.java
@@ -1,36 +1,48 @@
 package com.trevor.OffliNetflix;
 
 import com.trevor.OffliNetflix.Film.Film;
-import com.trevor.OffliNetflix.Util.ExtensionFilter;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.File;
-import java.io.FilenameFilter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class OffliNetflixApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// EnumerateDirectory();
 		SpringApplication.run(OffliNetflixApplication.class, args);
 	}
 
-	public static void EnumerateDirectory() {
-		File rootDirectory = new File(Film.getRootDir());
-		List<File> movieDirectories = List.of(rootDirectory.listFiles());
+	public static void EnumerateDirectory() throws IOException {
+		Stream<Path> walkStream = Files.walk(Paths.get(Film.getRootDir()));
+		List<Path> allFiles = walkStream.filter(p -> p.toFile().isFile()).toList();
+		List<Path> filmFiles = new ArrayList<>();
 
-		if (movieDirectories != null)
-			for (File f : movieDirectories)
-				System.out.println(f.getPath());
+		for (Path p : allFiles) {
+			String s = p.toString();
+			if (s.endsWith("mp4") || s.endsWith("mkv") || s.endsWith("avi")) {
+				String filmName = FilenameUtils.removeExtension(s);
+				System.out.printf("filmName: %s\n", s);
 
+				Matcher m = Pattern.compile("(.*)\\((\\d{4})\\)(.*)").matcher(filmName);
+				int filmReleaseYear = -1;
+				if (m.find())
+					filmReleaseYear = Integer.parseInt(m.group(2));
+				System.out.printf("filmReleaseYear: %d\n", filmReleaseYear);
+			}
+		}
 	}
-
-
 }
+
+
+
