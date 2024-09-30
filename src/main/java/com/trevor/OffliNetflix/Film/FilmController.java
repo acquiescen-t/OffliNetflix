06c25801182c26d034c480a14f15b0f539c759c7
Trevor <trevorchew.kh@gmail.com>
2024-09-30 13:32:50 +0800

    Update FilmController.java
    Added HttpStatus as part of the response.
    Added ability to handle requests based on filmId

--- src/main/java/com/trevor/OffliNetflix/Film/FilmController.java
+++ src/main/java/com/trevor/OffliNetflix/Film/FilmController.java
@@ -1,11 +1,17 @@
 package com.trevor.OffliNetflix.Film;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
+
+// Handles requests and responses. Delegates tasks of interaction with the DB via the FilmService class.
 
 @RestController
 @RequestMapping("api/v1/film")
@@ -19,7 +25,12 @@ public class FilmController {
     }
 
     @GetMapping
-    public List<Film> getFilms() {
-        return filmService.getFilms();
+    public ResponseEntity<List<Film>> getAllFilms() {
+        return new ResponseEntity<List<Film>>(filmService.getAllFilms(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Film>> getFilmById(@PathVariable Long id) {
+        return new ResponseEntity<Optional<Film>>(filmService.getFilmById(id), HttpStatus.OK);
     }
 }
