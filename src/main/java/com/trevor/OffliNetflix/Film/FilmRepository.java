f4830baa920876c4032ab1775763b4667a1bd889
Trevor <trevorchew.kh@gmail.com>
2024-10-01 13:04:20 +0800

    Update FilmRepository.java
    Fixed issue for search "not working"; film name is always in title case and search is always in lower case, added ignore case to search.

--- src/main/java/com/trevor/OffliNetflix/Film/FilmRepository.java
+++ src/main/java/com/trevor/OffliNetflix/Film/FilmRepository.java
@@ -3,6 +3,11 @@ package com.trevor.OffliNetflix.Film;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface FilmRepository extends JpaRepository<Film, Long> {
+
+      Optional<List<Film>> findByNameContainingIgnoreCase(String name);
 }
