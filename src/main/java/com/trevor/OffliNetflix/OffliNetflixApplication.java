dff0b7831f3801e379dc44b7585ba9b4629c50f2
Trevor <trevorchew.kh@gmail.com>
2024-10-02 13:01:36 +0800

    Update OffliNetflixApplication.java
    Optimised imports

--- src/main/java/com/trevor/OffliNetflix/OffliNetflixApplication.java
+++ src/main/java/com/trevor/OffliNetflix/OffliNetflixApplication.java
@@ -1,19 +1,9 @@
 package com.trevor.OffliNetflix;
 
-import com.trevor.OffliNetflix.Film.Film;
-import org.apache.commons.io.FilenameUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 @SpringBootApplication
 public class OffliNetflixApplication {
