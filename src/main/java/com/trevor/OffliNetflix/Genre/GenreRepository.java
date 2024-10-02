4941c1cc5d2c45c853c3cde5056e0d8db790ef6e
Trevor <trevorchew.kh@gmail.com>
2024-10-02 13:02:28 +0800

    Edited Genre and Star Repositories
    Added functions to search and get by names

--- src/main/java/com/trevor/OffliNetflix/Genre/GenreRepository.java
+++ src/main/java/com/trevor/OffliNetflix/Genre/GenreRepository.java
@@ -2,5 +2,12 @@ package com.trevor.OffliNetflix.Genre;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface GenreRepository extends JpaRepository<Genre, Long> {
+
+    Optional<List<Genre>> findByNameContainingIgnoreCase(String name);
+
+    Optional<Genre> getByNameIgnoreCase(String name);
 }
--- src/main/java/com/trevor/OffliNetflix/Star/StarRepository.java
+++ src/main/java/com/trevor/OffliNetflix/Star/StarRepository.java
@@ -2,5 +2,11 @@ package com.trevor.OffliNetflix.Star;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface StarRepository extends JpaRepository<Star, Long> {
+
+    Optional<List<Star>> findByNameContainingIgnoreCase(String name);
+    Optional<Star> getByNameIgnoreCase(String s);
 }
