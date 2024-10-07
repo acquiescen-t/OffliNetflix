5867579929903263da3782faf43f3ee9a3820ce0
Trevor <trevorchew.kh@gmail.com>
2024-10-07 15:32:37 +0800

    Update OffliNetflixApplication.java
    Fixed cors error

--- src/main/java/com/trevor/OffliNetflix/OffliNetflixApplication.java
+++ src/main/java/com/trevor/OffliNetflix/OffliNetflixApplication.java
@@ -2,6 +2,9 @@ package com.trevor.OffliNetflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.io.IOException;
 
@@ -10,6 +13,20 @@ public class OffliNetflixApplication {
 
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(OffliNetflixApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(false).maxAge(3600);
+			}
+		};
 	}
 }
 
