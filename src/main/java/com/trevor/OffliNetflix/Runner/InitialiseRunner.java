692a071c279b59e95149c0c60de3b2f8e1f5bc6e
Trevor <trevorchew.kh@gmail.com>
2024-11-18 15:04:26 +0800

    Update InitialiseRunner.java
    Commented function calls in Run() as data is already in place in the database

--- src/main/java/com/trevor/OffliNetflix/Runner/InitialiseRunner.java
+++ src/main/java/com/trevor/OffliNetflix/Runner/InitialiseRunner.java
@@ -31,28 +31,28 @@ public class InitialiseRunner implements CommandLineRunner {
 
     @Override
     public void run(String...args) throws Exception {
-        initialiseGenres();
-        initialiseStars();
-        initialiseFilms();
+//        initialiseGenres();
+//        initialiseStars();
+//        initialiseFilms();
     }
 
     public void initialiseGenres() {
-        Genre horror = new Genre("Horror");
-        Genre psychologicalHorror = new Genre("Psychological Horror");
-        Genre thriller = new Genre("Thriller");
-        Genre slasher = new Genre("Slasher");
-        Genre psychologicalThriller = new Genre("Psychological Thriller");
-        Genre romance = new Genre("Romance");
-        Genre scienceFiction = new Genre("Science Fiction");
-        Genre action = new Genre("Action");
-        Genre crime = new Genre("Crime");
-        Genre comedy = new Genre("Comedy");
-        Genre drama = new Genre("Drama");
-        Genre superhero = new Genre("Superhero");
-        Genre adventure = new Genre("Adventure");
-        Genre fantasy = new Genre("Fantasy");
-        Genre sport = new Genre("Sport");
-        Genre mystery = new Genre("Mystery");
+        Genre horror = new Genre("Horror".toLowerCase());
+        Genre psychologicalHorror = new Genre("Psychological Horror".toLowerCase());
+        Genre thriller = new Genre("Thriller".toLowerCase());
+        Genre slasher = new Genre("Slasher".toLowerCase());
+        Genre psychologicalThriller = new Genre("Psychological Thriller".toLowerCase());
+        Genre romance = new Genre("Romance".toLowerCase());
+        Genre scienceFiction = new Genre("Science Fiction".toLowerCase());
+        Genre action = new Genre("Action".toLowerCase());
+        Genre crime = new Genre("Crime".toLowerCase());
+        Genre comedy = new Genre("Comedy".toLowerCase());
+        Genre drama = new Genre("Drama".toLowerCase());
+        Genre superhero = new Genre("Superhero".toLowerCase());
+        Genre adventure = new Genre("Adventure".toLowerCase());
+        Genre fantasy = new Genre("Fantasy".toLowerCase());
+        Genre sport = new Genre("Sport".toLowerCase());
+        Genre mystery = new Genre("Mystery".toLowerCase());
 
         genreRepo.saveAll(List.of(
                 horror,
@@ -73,20 +73,18 @@ public class InitialiseRunner implements CommandLineRunner {
                 mystery
         ));
     }
-    public void initialiseStars() {
-        Star emilyBlunt = new Star("Emily Blunt");
-        Star johnKrasinski = new Star("John Krasinski");
-        Star daneDeHann = new Star("Dane DeHaan");
-        Star jasonIsaacs = new Star("Jason Isaacs");
-        Star miaGoth = new Star("Mia Goth");
+    public void initialiseStars() throws IOException {
+        Stream<Path> walkStream = Files.walk(Paths.get(Star.getRootDir()));
+        List<Path> allFiles = walkStream.filter(p -> p.toFile().isFile()).toList();
 
-        starRepo.saveAll(List.of(
-                emilyBlunt,
-                johnKrasinski,
-                daneDeHann,
-                jasonIsaacs,
-                miaGoth
-        ));
+        for (Path p : allFiles) {
+            String imageUrl = p.toString().replace(Film.getRootDir(), "http://localhost:8081/");
+            String name = p.getFileName().toString();
+            name = name.substring(0, name.length() - 4);
+            Star star = new Star(name, imageUrl);
+            System.out.println(star);
+            starRepo.save(star);
+        }
     }
     public void initialiseFilms() throws IOException {
         Stream<Path> walkStream = Files.walk(Paths.get(Film.getRootDir()));
@@ -95,31 +93,34 @@ public class InitialiseRunner implements CommandLineRunner {
         for (Path p : allFiles) {
             String s = p.toString();
             if (s.endsWith("mp4") || s.endsWith("mkv") || s.endsWith("avi")) {
-                Path fileName = p.getFileName();
-                System.out.printf("fileName: %s\n", fileName);
+                if (!s.contains("[Collection]") && !s.contains("web-assets")) {
+                    Path fileName = p.getFileName();
+                    System.out.printf("fileName: %s\n", fileName);
 
-                Matcher m = Pattern.compile("(.*)\\((\\d{4})\\)(.*)").matcher(fileName.toString());
-                int filmReleaseYear = -1;
-                if (m.find())
-                    filmReleaseYear = Integer.parseInt(m.group(2));
-                System.out.printf("filmReleaseYear: %d\n", filmReleaseYear);
+                    Matcher m = Pattern.compile("(.*)\\((\\d{4})\\)(.*)").matcher(fileName.toString());
+                    int filmReleaseYear = -1;
+                    if (m.find())
+                        filmReleaseYear = Integer.parseInt(m.group(2));
+                    System.out.printf("filmReleaseYear: %d\n", filmReleaseYear);
 
-                String directoryPath = s.substring(0, s.lastIndexOf('\\'));
-                System.out.printf("directoryPath: %s\n", directoryPath);
+                    String directoryPath = s.substring(0, s.lastIndexOf('\\'));
+                    System.out.printf("directoryPath: %s\n", directoryPath);
 
-                String filmPath = fileName.toString();
-                System.out.printf("filmPath: %s\n", filmPath);
+                    String filmPath = fileName.toString();
+                    System.out.printf("filmPath: %s\n", filmPath);
 
-                String filmName = filmPath
-                        .replaceAll(" \\(\\d{4}\\)", "")
-                        .replaceAll("(.mp4|.mkv|.avi)", "");
-                System.out.printf("filmName: %s\n", filmName);
+                    String filmName = filmPath
+                            .replaceAll(" \\(\\d{4}\\)", "")
+                            .replaceAll("(.mp4|.mkv|.avi)", "");
+                    System.out.printf("filmName: %s\n", filmName);
 
-                String posterPath = directoryPath.concat("\\poster.jpg").replace(Film.getRootDir(), "127.0.0.1:8081/");
-                System.out.printf("posterPath: %s\n", posterPath);
+                    String posterPath = directoryPath.concat("/poster.jpg").replace(Film.getRootDir(), "http://localhost:8081/");
+                    System.out.printf("posterPath: %s\n", posterPath);
 
-                filmRepo.save(new Film(filmName, filmReleaseYear, directoryPath, posterPath, filmPath));
-                return;
+                    String synopsis = "Synopsis placeholder";
+
+                    filmRepo.save(new Film(filmName, filmReleaseYear, directoryPath, posterPath, filmPath, synopsis));
+                }
             }
         }
     }
