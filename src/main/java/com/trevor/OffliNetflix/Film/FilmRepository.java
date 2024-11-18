f1d83771bbdb85f55ae83be3ca66c817f857b131
Trevor <trevorchew.kh@gmail.com>
2024-11-18 14:47:54 +0800

    Update FilmRepository.java
    1. Added option to find films by genre.
    2. Added option to find films by UUID.

--- src/main/java/com/trevor/OffliNetflix/Film/FilmRepository.java
+++ src/main/java/com/trevor/OffliNetflix/Film/FilmRepository.java
@@ -1,13 +1,17 @@
 package com.trevor.OffliNetflix.Film;
 
+import com.trevor.OffliNetflix.Genre.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface FilmRepository extends JpaRepository<Film, Long> {
 
       Optional<List<Film>> findByNameContainingIgnoreCase(String name);
+      Optional<List<Film>> findByGenresOfFilmIn(Optional<List<Genre>> genre);
+      Optional<Film> findById(UUID uuid);
 }
