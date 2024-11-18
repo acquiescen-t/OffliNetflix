8b7d6393481c9f73af848f10f893d6700443ca13
Trevor <trevorchew.kh@gmail.com>
2024-11-18 14:37:38 +0800

    Update FilmController.java
    1. Swapped from Long to UUID.
    2. Added path specificity to search for film by name
    3. Update genres and stars by UUID.

--- src/main/java/com/trevor/OffliNetflix/Film/FilmController.java
+++ src/main/java/com/trevor/OffliNetflix/Film/FilmController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 
 // Handles requests and responses. Delegates tasks of interaction with the DB via the FilmService class.
 
@@ -28,22 +29,27 @@ public class FilmController {
     }
 
     @GetMapping("/id/{id}")
-    public ResponseEntity<Optional<Film>> getFilmById(@PathVariable Long id) {
+    public ResponseEntity<Optional<Film>> getFilmById(@PathVariable UUID id) {
         return new ResponseEntity<Optional<Film>>(filmService.getFilmById(id), HttpStatus.OK);
     }
 
-    @GetMapping("/search/{name}")
+    @GetMapping("/search/film-name/{name}")
     public ResponseEntity<Optional<List<Film>>> getFilmsByName(@PathVariable String name) {
         return new ResponseEntity<Optional<List<Film>>>(filmService.getFilmsByName(name), HttpStatus.OK);
     }
 
+    @GetMapping("/search/genre-name/{genre}")
+    public ResponseEntity<Optional<List<Film>>> getFilmsByGenre(@PathVariable String genre) {
+        return new ResponseEntity<Optional<List<Film>>>(filmService.getFilmsByGenre(genre), HttpStatus.OK);
+    }
+
     @PostMapping("/update-genres")
     public ResponseEntity<Film> updateFilmGenres(@RequestBody Map<String, String> payload) {
-        return new ResponseEntity<Film>(filmService.updateGenres(payload.get("filmId"), payload.get("genres")), HttpStatus.CREATED);
+        return new ResponseEntity<Film>(filmService.updateGenres(UUID.fromString(payload.get("filmId")), payload.get("genres")), HttpStatus.CREATED);
     }
 
     @PostMapping("/update-stars")
     public ResponseEntity<Film> updateFilmStars(@RequestBody Map<String, String> payload) {
-        return new ResponseEntity<Film>(filmService.updateStars(payload.get("filmId"), payload.get("stars")), HttpStatus.CREATED);
+        return new ResponseEntity<Film>(filmService.updateStars(UUID.fromString(payload.get("filmId")), payload.get("stars")), HttpStatus.CREATED);
     }
 }
