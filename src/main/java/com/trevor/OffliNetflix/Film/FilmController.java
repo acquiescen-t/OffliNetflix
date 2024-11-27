1fd80ff05a7478f28d3355cb5e69a19d90a006b9
Trevor <trevorchew.kh@gmail.com>
2024-11-27 18:08:09 +0800

    Update FilmController.java
    1. Fixed http request methods
    2. Added way to update a film's synopsis and images

--- src/main/java/com/trevor/OffliNetflix/Film/FilmController.java
+++ src/main/java/com/trevor/OffliNetflix/Film/FilmController.java
@@ -1,5 +1,6 @@
 package com.trevor.OffliNetflix.Film;
 
+import com.trevor.OffliNetflix.TMDBFilm.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -48,13 +49,25 @@ public class FilmController {
         return new ResponseEntity<List<Film>>(filmService.getRandomFilms(count), HttpStatus.OK);
     }
 
-    @PostMapping("/update-genres")
+    @PutMapping("/update-genres")
     public ResponseEntity<Film> updateFilmGenres(@RequestBody Map<String, String> payload) {
         return new ResponseEntity<Film>(filmService.updateGenres(UUID.fromString(payload.get("filmId")), payload.get("genres")), HttpStatus.CREATED);
     }
 
-    @PostMapping("/update-stars")
+    @PutMapping("/update-stars")
     public ResponseEntity<Film> updateFilmStars(@RequestBody Map<String, String> payload) {
         return new ResponseEntity<Film>(filmService.updateStars(UUID.fromString(payload.get("filmId")), payload.get("stars")), HttpStatus.CREATED);
     }
+
+    @PutMapping("/update-film/{filmId}")
+    public ResponseEntity<Film> updateFilm(@PathVariable UUID filmId, @RequestBody Result result) {
+        System.out.println("result: " + result);
+        return new ResponseEntity<Film>(filmService.updateFilm(filmId, result), HttpStatus.OK);
+    }
+
+//    @GetMapping("/test-get-film/")
+//    public ResponseEntity<List<Film>> getFilmsByMostGenres() {
+//        return new ResponseEntity<List<Film>>(filmService.getFilmsByMostGenres(), HttpStatus.OK);
+//    }
+
 }
