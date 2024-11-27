0c445a384289788cde0f7242bf05b350d09ca7e4
Trevor <trevorchew.kh@gmail.com>
2024-11-27 18:11:31 +0800

    Update OffliNetflixApplication.java
    Removed corsConfigurer as spring security takes precedence

--- src/main/java/com/trevor/OffliNetflix/OffliNetflixApplication.java
+++ src/main/java/com/trevor/OffliNetflix/OffliNetflixApplication.java
@@ -2,9 +2,6 @@ package com.trevor.OffliNetflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.io.IOException;
 
@@ -13,20 +10,6 @@ public class OffliNetflixApplication {
 
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(OffliNetflixApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("*")
-						.allowedMethods("*")
-						.allowedHeaders("*")
-						.allowCredentials(false).maxAge(3600);
-			}
-		};
 	}
 }
 
