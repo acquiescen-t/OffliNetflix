a32f99e866a3d5323aafd4093cc1c9aa478ce01d
Trevor <trevorchew.kh@gmail.com>
2024-12-11 15:46:43 +0800

    Update FilmController.java
    Added logs.
    Implemented a method to find Films starring a certain Star.
    Implemented a method to find Films with the most number of Genres (used to test visual layout for index page).
    Implemented a method to update Stars of a certain Film.

--- src/main/java/com/trevor/OffliNetflix/Film/FilmController.java
+++ src/main/java/com/trevor/OffliNetflix/Film/FilmController.java
@@ -1,15 +1,13 @@
 package com.trevor.OffliNetflix.Film;
 
 import com.trevor.OffliNetflix.TMDBFilm.Result;
+import com.trevor.OffliNetflix.TMDBFilm.TMDBCredits;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 // Handles requests and responses. Delegates tasks of interaction with the DB via the FilmService class.
 
@@ -26,6 +24,7 @@ public class FilmController {
 
     @GetMapping
     public ResponseEntity<List<Film>> getAllFilms() {
+        System.out.println("Calling getAllFilms() in FilmController");
         return new ResponseEntity<List<Film>>(filmService.getAllFilms(), HttpStatus.OK);
     }
 
@@ -41,7 +40,14 @@ public class FilmController {
 
     @GetMapping("/search/genre-name/{genre}")
     public ResponseEntity<Optional<List<Film>>> getFilmsByGenre(@PathVariable String genre) {
+        System.out.printf("Calling getFilmsByGenre(%s) in FilmController.", genre);
         return new ResponseEntity<Optional<List<Film>>>(filmService.getFilmsByGenre(genre), HttpStatus.OK);
+    }
+
+    @GetMapping("/search/star-name/{star}")
+    public ResponseEntity<Optional<List<Film>>> getFilmsByStar(@PathVariable String star) {
+        System.out.printf("Calling getFilmsByStar(%s) in FilmController.", star);
+        return new ResponseEntity<Optional<List<Film>>>(filmService.getFilmsByStar(star), HttpStatus.OK);
     }
 
     @GetMapping("/get-random/{count}")
@@ -49,15 +55,27 @@ public class FilmController {
         return new ResponseEntity<List<Film>>(filmService.getRandomFilms(count), HttpStatus.OK);
     }
 
+    @GetMapping("/get-by-most-genres/{count}")
+    public ResponseEntity<List<Film>> getFilmsByMostGenres(@PathVariable int count) {
+        List<UUID> filmIds = filmService.getFilmsByMostGenres(count);
+        List<Film> filmsWithMostGenres = new ArrayList<>();
+        for(UUID filmId : filmIds) {
+            Optional<Film> film = filmService.getFilmById(filmId);
+            if (!film.isEmpty())
+                filmsWithMostGenres.add(film.get());
+        }
+        return new ResponseEntity<List<Film>>(filmsWithMostGenres, HttpStatus.OK);
+    }
+
     @PutMapping("/update-genres")
     public ResponseEntity<Film> updateFilmGenres(@RequestBody Map<String, String> payload) {
         return new ResponseEntity<Film>(filmService.updateGenres(UUID.fromString(payload.get("filmId")), payload.get("genres")), HttpStatus.CREATED);
     }
 
-    @PutMapping("/update-stars")
-    public ResponseEntity<Film> updateFilmStars(@RequestBody Map<String, String> payload) {
-        return new ResponseEntity<Film>(filmService.updateStars(UUID.fromString(payload.get("filmId")), payload.get("stars")), HttpStatus.CREATED);
-    }
+//    @PutMapping("/update-stars")
+//    public ResponseEntity<Film> updateFilmStars(@RequestBody Map<String, String> payload) {
+//        return new ResponseEntity<Film>(filmService.updateStars(UUID.fromString(payload.get("filmId")), payload.get("stars")), HttpStatus.CREATED);
+//    }
 
     @PutMapping("/update-film/{filmId}")
     public ResponseEntity<Film> updateFilm(@PathVariable UUID filmId, @RequestBody Result result) {
@@ -65,9 +83,9 @@ public class FilmController {
         return new ResponseEntity<Film>(filmService.updateFilm(filmId, result), HttpStatus.OK);
     }
 
-//    @GetMapping("/test-get-film/")
-//    public ResponseEntity<List<Film>> getFilmsByMostGenres() {
-//        return new ResponseEntity<List<Film>>(filmService.getFilmsByMostGenres(), HttpStatus.OK);
-//    }
-
+    @PutMapping("/{filmId}/update-stars")
+    public ResponseEntity<Film> updateFilmStars(@PathVariable UUID filmId, @RequestBody TMDBCredits credits) {
+        System.out.println("credits: " + credits);
+        return new ResponseEntity<Film>(filmService.updateStars(filmId, credits), HttpStatus.OK);
+    }
 }
