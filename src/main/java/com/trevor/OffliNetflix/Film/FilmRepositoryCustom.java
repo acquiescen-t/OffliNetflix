b65693d89bcd7f8fc09eef3467ac7934a7579d65
Trevor <trevorchew.kh@gmail.com>
2024-12-11 15:36:37 +0800

    Added repo for more complex queries
    Added logs.
    Implemented a method to find films starring a certain Star.
    Implemented a method to find films with the most number of genres (used to test visual layout).
    Disabled the old method of updating all of a film's data at once, separating into the below methods:
    1. Implemented a method to update the stars of a certain film.
    2. Implemented a method to update the images and synopsis of a certain film.

---
+++ src/main/java/com/trevor/OffliNetflix/Film/FilmRepositoryCustom.java
@@ -0,0 +1,48 @@
+package com.trevor.OffliNetflix.Film;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class FilmRepositoryCustom {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<Object[]> getFilmsByStar(String star) {
+        String nativeQuery =
+                """
+                SELECT release_year, f.id, directory_path, film_path, image_url, name, synopsis, backdrop_url 
+                FROM star s 
+                INNER JOIN film_stars fs
+                ON s.id = fs.star_id
+                INNER JOIN film f 
+                ON f.id = fs.film_id 
+                WHERE UPPER(s.searchable_name) LIKE '
+                """.concat(star).concat("';");
+
+        Query query = entityManager.createNativeQuery(nativeQuery);
+        return query.getResultList();
+    }
+    public List<Object[]> getFilmsByMostGenres(int count) {
+        String nativeQuery = "SELECT release_year, f.id, directory_path, film_path, image_url, name, synopsis, backdrop_url " +
+                "FROM Film f " +
+                "INNER JOIN ( " +
+                "    SELECT fg.film_id, COUNT(*) AS genre_count " +
+                "    FROM film_genres fg " +
+                "    INNER JOIN Genre g ON fg.genre_id = g.id " +
+                "    GROUP BY fg.film_id " +
+                "    ORDER BY genre_count DESC " +
+                "    LIMIT :limitCount" +
+                ") subquery ON f.id = subquery.film_id";
+
+        Query query = entityManager.createNativeQuery(nativeQuery);
+        query.setParameter("limitCount", count);
+
+        return query.getResultList();
+    }
+}
--- src/main/java/com/trevor/OffliNetflix/Film/FilmService.java
+++ src/main/java/com/trevor/OffliNetflix/Film/FilmService.java
@@ -5,6 +5,8 @@ import com.trevor.OffliNetflix.Genre.GenreRepository;
 import com.trevor.OffliNetflix.Star.Star;
 import com.trevor.OffliNetflix.Star.StarRepository;
 import com.trevor.OffliNetflix.TMDBFilm.Result;
+import com.trevor.OffliNetflix.TMDBFilm.TMDBCredits;
+import com.trevor.OffliNetflix.TMDBFilm.TMDBStar;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,17 +18,21 @@ import java.util.*;
 public class FilmService {
 
     private final FilmRepository filmRepository;
+
+    private final FilmRepositoryCustom filmRepositoryCustom;
     private final GenreRepository genreRepository;
     private final StarRepository starRepository;
 
     @Autowired
-    public FilmService(FilmRepository filmRepository, GenreRepository genreRepository, StarRepository starRepository) {
+    public FilmService(FilmRepository filmRepository, FilmRepositoryCustom filmRepositoryCustom, GenreRepository genreRepository, StarRepository starRepository) {
         this.filmRepository = filmRepository;
+        this.filmRepositoryCustom = filmRepositoryCustom;
         this.genreRepository = genreRepository;
         this.starRepository = starRepository;
     }
 
     public List<Film> getAllFilms() {
+        System.out.println("Calling filmRepository.findAll() in getAllFilms()");
         return filmRepository.findAll();
     }
 
@@ -39,6 +45,7 @@ public class FilmService {
     }
 
     public Optional<List<Film>> getFilmsByGenre(String genre) {
+        System.out.printf("Calling getFilmsByGenre(%s) in FilmService", genre);
         Optional<List<Genre>> g = genreRepository.findByNameContainingIgnoreCase(genre);
 
         if (g.isPresent()) {
@@ -46,6 +53,27 @@ public class FilmService {
         }
 
         return filmRepository.findByGenresOfFilmIn(g);
+    }
+
+    public Optional<List<Film>> getFilmsByStar(String star) {
+        System.out.printf("Calling getFilmsByStar(%s) in FilmService\n", star);
+        Optional<List<Star>> s = starRepository.findBySearchableNameContainingIgnoreCase(star);
+
+        if (s.isPresent()) {
+            s.get().stream().forEach(System.out::print);
+        }
+
+        return filmRepository.findByStarsOfFilmIn(s);
+    }
+
+    public List<UUID> getFilmsByMostGenres(int count) {
+        List<Object[]> filmsWithMostGenres = filmRepositoryCustom.getFilmsByMostGenres(count);
+        List<UUID> filmIds = new ArrayList<>();
+        for (Object[] objArr : filmsWithMostGenres) {
+            UUID filmId = (UUID)objArr[1];
+            filmIds.add(filmId);
+        }
+        return filmIds;
     }
 
     public List<Film> getRandomFilms(int count) {
@@ -66,37 +94,31 @@ public class FilmService {
 
         return film.get();
     }
-/*
-    public Optional<List<Film>> updateInformation(UUID[] filmsId, String[] filmsGenres, String[] filmsStars) {
-        if (filmsId.length != filmsGenres.length || filmsGenres.length != filmsStars.length) {
-            String message = "Mismatch between filmsId.length ["
-                    .concat(String.valueOf(filmsId.length))
-                    .concat("], filmsGenre.length [")
-                    .concat(String.valueOf(filmsGenres.length))
-                    .concat("] and filmsStars.length [")
-                    .concat(String.valueOf(filmsStars.length))
-                    .concat("]!");
-            throw new IllegalStateException(message);
-        }
 
-        int count = filmsId.length;
-        Optional<List<Film>> updatedFilms = null;
+    public Film updateStars(UUID filmId, TMDBCredits credits) {
+        System.out.println("Calling updateFilm with:");
+        System.out.printf("UUID: %s\n", filmId);
+        System.out.printf("credits: %s\n", credits);
 
-        for (int loop = 0; loop < count; loop++) {
-            updateGenres(filmsId[loop], filmsGenres[loop]);
-            Film updatedFilm = updateStars(filmsId[loop], filmsStars[loop]);
-            updatedFilms.get().add(updatedFilm);
-        }
-
-        return updatedFilms;
-    }
-*/
-    public Film updateStars(UUID filmId, String stars) {
         Optional<Film> film = getFilmById(filmId);
         if (film.isEmpty())
             throw new IllegalStateException("Film '" + film + "' not found!");
 
-        Set<Star> starSet = Star.extractStars(stars, starRepository);
+        TMDBStar[] cast = credits.getCast();
+        Set<Star> starSet = new HashSet<>();
+        for (int loop = 0; loop < cast.length; loop++) {
+            Star star = new Star(cast[loop].getName(), cast[loop].getProfile_path(), cast[loop].getName().replaceAll(" ", "-"));
+            System.out.printf("Current star: %s\n", star);
+            Optional<Star> starLookup = starRepository.getByNameIgnoreCase(star.getName());
+            if (starLookup.isEmpty()) {
+                System.out.printf("Star %s does not exist in database, adding them in.\n", star.getName());
+                Star newStar = starRepository.save(star);
+                starSet.add(newStar);
+            } else {
+                System.out.printf("Star %s already exists in database, not adding them in.\n", star.getName());
+                starSet.add(starLookup.get());
+            }
+        }
 
         film.get().setStarsOfFilm(starSet);
         filmRepository.save(film.get());
@@ -113,21 +135,30 @@ public class FilmService {
         if (film.isEmpty())
             throw new IllegalStateException("Film '" + film + "' not found!");
 
-        film.get().setBackdropUrl(result.getBackdrop_path());
-        film.get().setImageUrl(result.getPoster_path());
-        film.get().setSynopsis(result.getOverview());
-
         System.out.printf("result.backdrop_path: %s\n", result.getBackdrop_path());
-        System.out.printf("result.poster_path: %s\n", result.getPoster_path());
-        System.out.printf("result.overview: %s\n", result.getOverview());
+        film.get().setBackdropUrl(result.getBackdrop_path());
 
+        System.out.printf("result.poster_path: %s\n", result.getPoster_path());
+        film.get().setImageUrl(result.getPoster_path());
+
+        System.out.printf("result.overview: %s\n", result.getOverview());
+        film.get().setSynopsis(result.getOverview());
+        film.get().setTmdb_id(result.getId());
+
+        Set<Genre> updatedGenres = new HashSet<>();
+        int[] genreIds = result.getGenre_ids();
+        for (int gid : genreIds) {
+            Optional<Genre> genre = genreRepository.getByTmdbId(gid);
+            if (genre.isPresent())
+                updatedGenres.add(genre.get());
+        }
+        System.out.printf("result.genre_ids: \n");
+        for(int i : result.getGenre_ids()) {
+            System.out.printf("%d\n", i);
+        }
+        film.get().setGenresOfFilm(updatedGenres);
 
         filmRepository.save(film.get());
         return film.get();
     }
-
-//    public List<Film> getFilmsByMostGenres() {
-//        List<Film> filmsWithMostGenres = filmRepository.getFilmsByMostGenres();
-//        return filmsWithMostGenres;
-//    }
 }
