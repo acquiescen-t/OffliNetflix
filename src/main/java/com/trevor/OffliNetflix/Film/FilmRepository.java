b9dcc1c3d194ec02db4527f7b60d81d46a92dd72
Trevor <trevorchew.kh@gmail.com>
2024-12-11 15:47:00 +0800

    Update FilmRepository.java
    Implemented a method to find Films featuring a certain Star

--- src/main/java/com/trevor/OffliNetflix/Film/FilmRepository.java
+++ src/main/java/com/trevor/OffliNetflix/Film/FilmRepository.java
@@ -1,6 +1,7 @@
 package com.trevor.OffliNetflix.Film;
 
 import com.trevor.OffliNetflix.Genre.Genre;
+import com.trevor.OffliNetflix.Star.Star;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,10 +9,12 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+
 @Repository
 public interface FilmRepository extends JpaRepository<Film, Long> {
 
       Optional<List<Film>> findByNameContainingIgnoreCase(String name);
       Optional<List<Film>> findByGenresOfFilmIn(Optional<List<Genre>> genre);
+      Optional<List<Film>> findByStarsOfFilmIn(Optional<List<Star>> star);
       Optional<Film> findById(UUID uuid);
 }
